01174f2d83b037c6ad211fa9aa871e4027b97973
mtamoto <m-tamoto@bravetact.com>
2025-01-17 14:00:35 +0900

    ユーザー側の料理確認画面

--- trunk/TAKETORI/src/main/java/com/taketoritei/order/dining/controller/DiningController.java
+++ trunk/TAKETORI/src/main/java/com/taketoritei/order/dining/controller/DiningController.java
@@ -3,45 +3,24 @@ package com.taketoritei.order.dining.controller;
 import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.commons.lang3.StringUtils;
-import org.jooq.Record;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.taketoritei.order.common.Consts;
-import com.taketoritei.order.common.DateUtil;
-import com.taketoritei.order.common.controller.AdminBaseController;
 import com.taketoritei.order.common.controller.FrontBaseController;
-import com.taketoritei.order.config.IllegalException;
 import com.taketoritei.order.dining.form.AdminDiningForm;
-import com.taketoritei.order.dining.form.AdminDiningMessageForm;
 import com.taketoritei.order.dining.service.DiningService;
-import com.taketoritei.order.jooq.tables.records.DDiningMessageRecord;
-import com.taketoritei.order.jooq.tables.records.DDiningRecord;
-import com.taketoritei.order.jooq.tables.records.MDiningPlaceRecord;
 import com.taketoritei.order.login.form.User;
 import com.taketoritei.order.room.service.RoomService;
 
 /**
- * 朝食・夕食管理
+ * 朝食・夕食確認
  */
 @Controller
 public class DiningController extends FrontBaseController {
